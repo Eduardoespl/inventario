fb882d2f2c25333b9a3f53ceb742a5c9623655d0
Eduardoespl <eduardoesp3317@gmail.com>
2024-06-05 14:03:35 -0600

    Updated files

---
+++ app/controls/RoundButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Title } from "react-native-paper";
+type Props ={
+    title: string;
+};
+const RoundButton = ({title}:Props): React.JSX.Element => (
+    <TouchableOpacity>
+        <Text>{title}</Text>
+    </TouchableOpacity>
+);
+export default RoundButton;
